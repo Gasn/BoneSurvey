6be688444d90844c2f5fb807b18b55b606277932
Sang Luu <sluu@palo-it.com>
2018-05-31 00:10:08 +1000

    Update all require fields

--- components/Form.js
+++ components/Form.js
@@ -1,27 +1,39 @@
 import React from "react";
-import { View, Text, TextInput, Picker } from "react-native";
+import { View, Text, TextInput, Button } from "react-native";
 import { CheckBox } from "react-native-elements";
+import { ageOptions } from "./Shared/AgeOptions";
+import { fractureOptions } from "./Shared/FractureOptions";
+import SelectInput from "react-native-select-input-ios";
 
 class Form extends React.Component {
   constructor() {
     super();
     this.state = {
       name: "",
-      sex: ""
+      sex: "",
+      age: 0,
+      fractures: 0,
+      falls: 0,
+      bmd: false,
+      tscore: "",
+      densitometer: "lunar",
+      bmdMeasure: ""
     };
   }
+  calculate() {}
   render() {
     return (
       <View style={styles.form}>
         <Text style={styles.label}>Full Name (optional)</Text>
         <TextInput
           style={styles.input}
+          placeholder="Name"
           autoCapitalize="words"
           onChangeText={name => this.setState({ name })}
           value={this.state.name}
         />
         <Text style={styles.label}>Sex</Text>
-        <View style={styles.flexrow}>          
+        <View style={styles.flexRow}>
           <CheckBox
             left
             containerStyle={styles.checkbox}
@@ -42,6 +54,97 @@ class Form extends React.Component {
           />
         </View>
         <Text style={styles.label}>Age</Text>
+        <SelectInput
+          style={styles.select}
+          options={ageOptions}
+          value={this.state.age}
+          onSubmitEditing={age => this.setState({ age })}
+        />
+        <Text style={styles.label}>Fractures since the age of 50</Text>
+        <SelectInput
+          style={styles.select}
+          options={fractureOptions}
+          value={this.state.fractures}
+          onSubmitEditing={fractures => this.setState({ fractures })}
+        />
+        <Text style={styles.label}>Falls over last 12 months</Text>
+        <SelectInput
+          style={styles.select}
+          options={fractureOptions}
+          value={this.state.falls}
+          onSubmitEditing={falls => this.setState({ falls })}
+        />
+        <Text style={styles.label}>
+          Do you have a Bone Mineral Density (BMD) measurement?
+        </Text>
+        <View style={styles.flexRow}>
+          <CheckBox
+            left
+            containerStyle={styles.checkbox}
+            title="Yes"
+            checkedIcon="dot-circle-o"
+            uncheckedIcon="circle-o"
+            onPress={() => this.setState({ bmd: true })}
+            checked={this.state.bmd}
+          />
+          <CheckBox
+            left
+            containerStyle={styles.checkbox}
+            title="No"
+            checkedIcon="dot-circle-o"
+            uncheckedIcon="circle-o"
+            onPress={() => this.setState({ bmd: false })}
+            checked={!this.state.bmd}
+          />
+        </View>
+        <Text style={styles.label}>T-scores</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="T-scores"
+          autoCapitalize="none"
+          onChangeText={tscore => this.setState({ tscore })}
+          value={this.state.tscore}
+        />
+        <Text style={styles.label}>Densitometer</Text>
+        <View style={styles.flexRow}>
+          <CheckBox
+            left
+            containerStyle={styles.checkbox}
+            title="by DXA GE Lunar"
+            checkedIcon="dot-circle-o"
+            uncheckedIcon="circle-o"
+            onPress={() => this.setState({ densitometer: "lunar" })}
+            checked={this.state.densitometer === "lunar"}
+          />
+          <CheckBox
+            left
+            containerStyle={styles.checkbox}
+            title="by DXA Hologic"
+            checkedIcon="dot-circle-o"
+            uncheckedIcon="circle-o"
+            onPress={() => this.setState({ densitometer: "hologic" })}
+            checked={this.state.densitometer === "hologic"}
+          />
+        </View>
+        <Text style={styles.label}>Actual BMD</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="BMD"
+          autoCapitalize="none"
+          onChangeText={bmdMeasure => this.setState({ bmdMeasure })}
+          value={this.state.bmdMeasure}
+        />
+        <Text style={styles.label}>Disclaimer</Text>
+        <Text style={styles.disclaimer}>
+          The results produced by our calculator should serve as a guide only.
+          If concerned about your fracture risk, it is also important to consult
+          your doctor or a bone specialist.
+        </Text>
+        <Button
+          style={styles.button}
+          title="Calculate Risk Factor"
+          onPress={() => this.calculate}
+        />
       </View>
     );
   }
@@ -52,7 +155,8 @@ const styles = {
     flex: 1,
     flexDirection: "column",
     justifyContent: "flex-start",
-    alignSelf: "stretch"
+    alignSelf: "stretch",
+    marginBottom: 50
   },
   label: {
     marginBottom: 3,
@@ -66,7 +170,7 @@ const styles = {
     borderBottomWidth: 1,
     marginBottom: 30
   },
-  flexrow: {
+  flexRow: {
     flex: 1,
     flexDirection: "row",
     justifyContent: "flex-start",
@@ -80,6 +184,17 @@ const styles = {
     width: 100,
     padding: 0,
     marginTop: 10
+  },
+  select: {
+    borderWidth: 1,
+    borderRadius: 3,
+    marginTop: 5,
+    paddingTop: 8,
+    marginBottom: 25,
+    paddingBottom: 8
+  },
+  disclaimer: {
+    marginBottom: 20
   }
 };
 
--- components/Home.js
+++ components/Home.js
@@ -1,31 +1,54 @@
-import React,{Component} from 'react'
-import {ScrollView, Text} from 'react-native'
-import Form from './Form'
+import React, { Component } from "react";
+import { ScrollView, Text, KeyboardAvoidingView } from "react-native";
+import Form from "./Form";
 
 class Home extends Component {
-    static navigationOptions = {
-      title: 'Fracture Risk Calculator',
-    }; 
-    render() {
-      return (
-        <ScrollView style={styles.container}>
-          <Text style={styles.title}>Fill out the following to estimate your fracture risk</Text>
-          <Form/>
+  static navigationOptions = {
+    title: "Fracture Risk Calculator"
+  };
+  render() {
+    return (
+      <KeyboardAvoidingView
+        style={styles.container}
+        behavior="padding"
+        enabled
+        keyboardVerticalOffset={65}
+      >
+        <Text style={styles.title}>
+          Fill out the following to estimate your fracture risk
+        </Text>
+        <ScrollView
+          style={styles.scroll}
+          ref={ref => (this.scrollView = ref)}
+          onContentSizeChange={(contentWidth, contentHeight) => {
+            this.scrollView.scrollToEnd({ animated: false });
+          }}
+        >
+          <Form />
         </ScrollView>
-      );
-    }
+      </KeyboardAvoidingView>
+    );
   }
+}
 
-  const styles = {
-    container: {
-      flex: 1,
-      padding: 20
-    },
-    title: {
-      fontWeight: 'bold',
-      marginBottom: 20,
-      fontSize: 17
-    }
+const styles = {
+  container: {
+    flex: 1
+  },
+  title: {
+    fontWeight: "bold",
+    marginBottom: 20,
+    fontSize: 17,
+    paddingLeft: 20,
+    paddingRight: 20,
+    paddingTop: 20
+  },
+  scroll: {
+    flex: 1,
+    paddingLeft: 20,
+    paddingRight: 20,
+    flexGrow: 1
   }
+};
 
-  export default Home;
+export default Home;
